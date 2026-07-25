21ac5f5d4b20168b531fb56f19de34b9852ced7d
PabloDuranLopez <PabloDuran@ciencias.unam.mx>
2026-07-25 00:14:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ st.set_page_config(
 
 st.write("Hola soy Pablo Duran, soy estudiante de Actuaría y Matemáticas en la Universidad Nacional Autónoma de México (UNAM), con profundo interes en Machine Learning, Estadística Bayesiana y las Finanzas Cuantitativas. Me gusta construir modelos desde cero para entender a profundidad cómo funcionan. He desarrollado implementaciones propias de prediccion (tradicional y enfoque bayesiano) de despacho en incidentes viales del C5, prediccion de tumores malignos, diabetes, asi como el pricing de derivados financieros")
 st.write("Este es un proyecto mas, siendo compartido con ustedes de forma educativa, y que claro este proyecto no es una sugerencia de apuestas")
+st.write("Aun faltas cosas como la implementacion bayesiana, Under/Over, top marcadores probables, ¿Ambos equipos anotan?")
 
 c1, c2, c3 = st.columns([1,2,1])
 
