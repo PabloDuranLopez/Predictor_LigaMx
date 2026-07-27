6318c91e68952b2d4301d030c60f559bc3a5d05d
PabloDuranLopez <PabloDuran@ciencias.unam.mx>
2026-07-26 18:13:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ El desarrollo fue realizado como proyecto personal con fin educativo para profun
 **Las predicciones no constituyen recomendaciones de apuesta.**
 """)
 
-c1, c2, c3 = st.columns([1,2,1])
+c1, c2, c3 = st.columns([3,2,3])
 
 with c2:
     st.image("escudos/ligamx.png", width=250)
