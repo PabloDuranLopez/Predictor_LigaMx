f983048e6781be71ea26cb3ab7c4e1e85376a644
PabloDuranLopez <PabloDuran@ciencias.unam.mx>
2026-07-28 15:40:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ El desarrollo fue realizado como proyecto personal con fin educativo para profun
 
 **Las predicciones no constituyen recomendaciones de apuesta.**
 """)
+sr.markdown("Ya estan actualizados las predicciones de la J3!!!!")
 
 c1, c2, c3 = st.columns([1,2,1])
 
